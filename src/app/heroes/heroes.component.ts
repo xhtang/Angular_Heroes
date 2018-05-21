e25b22e35bf90b97833e262bf869070f097a92c4
xhtang <15302010031@fudan.edu.cn>
2018-05-21 19:40:45 +0800

    third commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import {HEROES} from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,15 +9,22 @@ import { Hero } from '../Hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
 
   hero: Hero = {
     id : 1,
     name: 'Jill'
-  }
+  };
+
+  heroes = HEROES;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero} from './Hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Alice' },
+  { id: 12, name: 'Bob' },
+  { id: 13, name: 'Klein' },
+  { id: 14, name: 'David' },
+  { id: 15, name: 'E' },
+  { id: 16, name: 'Jill' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
