9ba977939dc941099c8641c93869f71c14e62035
xhtang <15302010031@fudan.edu.cn>
2018-05-23 20:19:49 +0800

    route completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,6 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
 
   heroes: Hero[];
 
@@ -17,10 +16,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
